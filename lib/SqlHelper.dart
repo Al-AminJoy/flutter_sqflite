e243f0c14e5bb0361cb2c366cd06e1611b6f6a6e
Al-AminJoy <alaminislam3555@gmail.com>
2023-02-18 22:12:19 +0600

    CRUD Added

--- lib/SqlHelper.dart
+++ lib/SqlHelper.dart
@@ -1,42 +1,63 @@
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/sqflite.dart' as sql;
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-class SQLHelper{
-
+class SQLHelper {
   static const DB_NAME = "myDatabase.db";
   static const VERSION = 1;
 
   static final SQLHelper instance = SQLHelper();
 
-  static Database? database;
+  static  Database? database;
 
-
-
-  Future<Database?> getDatabase() async{
-    if(database != null){
+  Future<Database?> getDatabase() async {
+    if (database != null) {
       return database;
     }
 
     database = await initDb();
     return database;
-}
+  }
 
-  Future<Database> initDb() async  {
-
+  Future<Database> initDb() async {
     Directory directory = await getApplicationDocumentsDirectory();
-    String path = join(directory.path,DB_NAME);
+    String path = join(directory.path, DB_NAME);
 
-    return await openDatabase(path,version: VERSION, onCreate: onCreate);
-
+    return await openDatabase(path, version: VERSION, onCreate: onCreate);
   }
 
-  Future onCreate(Database db,int version) async{
-    db.execute("CREATE TABLE Person (id INTEGER PRIMARY KEY,name TEXT NOT NULL)");
+  Future onCreate(Database db, int version) async {
+    db.execute(
+        "CREATE TABLE Person (id INTEGER PRIMARY KEY,name TEXT NOT NULL)");
   }
 
+  Future<int> insert(Map<String, dynamic> row) async {
+    Database? db = await getDatabase();
+    return await db!.insert("Person", row);
+  }
 
+  Future<List<Map<String, dynamic>>> read() async {
+    Database? db = await getDatabase();
+    return await db!.query("Person");
+  }
+
+  Future<int> update(Map<String, dynamic> row) async {
+    Database? db = await getDatabase();
+    int id = row['id'];
+    //print(id);
+    //var id;
+    return await db!.update("Person", row, where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> delete(int id) async{
+    Database? db = await instance.getDatabase();
+
+    return await db!.delete("Person",where: 'id = ?',whereArgs: [id]);
+
+  }
 
 }
+
+
+
--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
 
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_sqflite/SqlHelper.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -21,13 +24,28 @@ class _HomeState extends State<Home> {
           child: Column(
             children: [
               const SizedBox(height: 10,),
-              ElevatedButton(onPressed: (){}, child: const Text("Create")),
+              ElevatedButton(onPressed: ()async{
+                Map<String,dynamic> data = HashMap();
+                data['name'] = "Al-Amin";
+                data['name'] = "Joy";
+                await SQLHelper.instance.insert(data);
+              }, child: const Text("Create")),
               const SizedBox(height: 10,),
-              ElevatedButton(onPressed: (){}, child: const Text("Read")),
+              ElevatedButton(onPressed: () async{
+                var dbQuery = await SQLHelper.instance.read();
+                print(dbQuery);
+              }, child: const Text("Read")),
               const SizedBox(height: 10,),
-              ElevatedButton(onPressed: (){}, child: const Text("Update")),
+              ElevatedButton(onPressed: ()async{
+                Map<String,dynamic> data = HashMap();
+                data['id'] = 1;
+                data['name'] = "Al-Amin Joy";
+                await SQLHelper.instance.update(data);
+              }, child: const Text("Update")),
               const SizedBox(height: 10,),
-              ElevatedButton(onPressed: (){}, child: const Text("Delete"))
+              ElevatedButton(onPressed: ()async{
+                await SQLHelper.instance.delete(1);
+              }, child: const Text("Delete"))
             ],
           ),
         ),
